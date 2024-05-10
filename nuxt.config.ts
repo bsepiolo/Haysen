1a6a587c061017d519ab052d116265ad497bc1b7
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2024-05-10 21:20:37 +0200

    styling added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   typescript: {
-    typeCheck: true,
+    typeCheck: true
   },
 
-  modules: ["@nuxt/eslint"],
-});
+  modules: ['@nuxt/eslint'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/_colors.scss" as *;'
+        }
+      }
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">
+    <div class="test__title">test</div>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+.test {
+  &__title {
+    background: $orange;
+  }
+}
+</style>
