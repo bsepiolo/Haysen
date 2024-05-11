aba1e5addfe124dd01e864a652ae23f2aa7bb1ac
Bartosz Sepioło <bartosz.sepiolo@hotmail.com>
2024-05-12 00:22:01 +0200

    size classes, v-model

--- components/TextBox.vue
+++ components/TextBox.vue
@@ -1,16 +1,52 @@
 <template>
-  <div>
-    {{ type }}
-    <input :type="type" :name="name" :placeholder="placeholder" />
+  <div class="text-box" :class="textBoxSizeClass[size]">
+    <input
+      v-model="model"
+      class="text-box__input"
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+    />
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-defineProps<{
+import { ref, computed } from 'vue'
+type TextBoxSize = 'sm' | 'md' | 'lg'
+interface TextBoxProps {
   type: string
   name: string
   placeholder: string
-}>()
+  size?: TextBoxSize
+}
 
-const count = ref(0)
+withDefaults(defineProps<TextBoxProps>(), {
+  size: 'lg'
+})
+
+const textBoxSizeClass: Record<TextBoxSize, string> = {
+  sm: 'text-box__small',
+  md: 'text-box__medium',
+  lg: 'text-box__large'
+}
+
+const model = defineModel<string | number>()
 </script>
+<style lang="scss" scoped>
+.text-box {
+  background: $gray-200;
+  border-radius: 8px;
+
+  &__large {
+    height: 56px;
+  }
+
+  &__input {
+    height: 100%;
+    width: 100%;
+    background: transparent;
+    border: none;
+    border-radius: 8px;
+    padding: 0 16px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="test">
-    <div class="test__title">
-      <TextBox name="test" type="1" placeholder="test" />
-    </div>
+  <div class="container">
+    <TextBox v-model="testValue" name="test" type="1" placeholder="test" />
   </div>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import TextBox from '@/components/TextBox.vue'
+const testValue = ref('')
 </script>
 <style lang="scss" scoped>
 .test {
